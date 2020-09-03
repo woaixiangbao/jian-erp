2ad63327a56ffa43d7270ae063374fe38959313b
jianqing <jianqing@luojilab.com>
2020-09-03 17:58:08 +0800

    delete vue init files and add login register views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../views/users/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +11,14 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'Login',
+    component: Home,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/users/Register.vue'),
   },
 ];
 
