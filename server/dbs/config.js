65210706a7d5607fc7430d83e2fcf7f4c3290c6f
jianqing <jianqing@luojilab.com>
2020-09-04 10:19:06 +0800

    开始服务端开发

---
+++ server/dbs/config.js
@@ -0,0 +1,38 @@
+// 数据库和邮箱配置文件
+const dbs = 'mongodb://127.0.0.1:27017/testjianqing';
+const redis = {
+  get host() {
+    return '127.0.0.1';
+  },
+  get port() {
+    return 6379;
+  },
+};
+const smtp = {
+  get host() {
+    return 'smtp.qq.com';
+  },
+  get user() {
+    return '81304209@qq.com'; // qq邮箱名
+  },
+  get pass() {
+    return '13995967441'; // qq邮箱授权码
+  },
+  // 邮箱验证码
+  get code() {
+    return () => Math.random()
+      .toString(16)
+      .slice(2, 6)
+      .toUpperCase();
+  },
+  // 定义验证码过期时间rules，5分钟内邮箱
+  get expire() {
+    return () => new Date().getTime() + 5 * 60 * 1000;
+  },
+};
+
+module.exports = {
+  dbs,
+  redis,
+  smtp,
+};
